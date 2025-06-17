b2236a5edc6a2c32b157bea7ee98abd81cc7cafb
Elwynn Chen <ooer@kanaries.net>
2025-06-18 02:14:47 +0800

    fix(docs): transpile local package

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  transpilePackages: ['@kanaries/ml'],
   images: {
     remotePatterns: [
       {
